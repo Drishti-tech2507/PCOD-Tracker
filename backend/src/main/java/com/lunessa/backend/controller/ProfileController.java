94baf1765e1bd8b1d9a4f32e4a88d088874d8f19
Drishti Chopra <drishtic250706@gmail.com>
2026-06-30 12:29:40 +0530

    Update ProfileController.java

--- backend/src/main/java/com/lunessa/backend/controller/ProfileController.java
+++ backend/src/main/java/com/lunessa/backend/controller/ProfileController.java
@@ -1,30 +1,25 @@
 package com.lunessa.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.lunessa.backend.model.User;
 import com.lunessa.backend.repository.UserRepository;
 
 @RestController
 @RequestMapping("/api/profile")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class ProfileController {
 
     @Autowired
     private UserRepository userRepository;
 
     @GetMapping("/{email}")
-    public User getProfile(
-            @PathVariable String email
-    ) {
+    public User getProfile(@PathVariable String email){
 
         return userRepository
                 .findByEmail(email)
                 .orElse(null);
     }
+
 }
